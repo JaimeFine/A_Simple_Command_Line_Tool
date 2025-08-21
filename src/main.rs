e4d1ab01ffdb7ef67e9ad16f7bc2e135d536fa7b
Jaime Fine <jaimefine6@gmail.com>
2025-08-21 18:38:19 +0800

    This should work!!!

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let config = Command::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
-    });
+    }); // Building the configuration arguments.
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
